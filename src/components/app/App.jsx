044c41cb98cc3cd24b8592ccb10753ac9b2b1642
Grzegorz Wójcik <hirashi7@gmail.com>
2020-10-18 16:58:11 +0200

    base routing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+/* Views */
+import Home from '../../views/home/Home.jsx';
+import About from '../../views/about/About.jsx';
+
+/* Components */
+import Header from 'components/layout/header/Header.jsx';
+
 import './App.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>TEST</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
